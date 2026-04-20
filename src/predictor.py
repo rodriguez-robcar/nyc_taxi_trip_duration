5668d9d947c7fd815fe76ec359dd72c1342c9922
Roberto Carlos Rodríguez Torres <robertocarlos@MacBook-Air-de-Roberto.local>
2026-04-19 18:02:49 -0600

    modified target to use log-transform

--- src/predictor.py
+++ src/predictor.py
@@ -1,10 +1,12 @@
+import numpy as np
+
 def predictor(model, train_data, test_data, features):
     
     X_test = test_data[features]
 
-    model.fit(train_data[features], train_data["trip_duration"])
+    model.fit(train_data[features], np.log1p(train_data["trip_duration"]))
 
-    Y_pred_test = model.predict(X_test)
+    Y_pred_test = np.expm1(model.predict(X_test))
 
     test_data["trip_duration"] = Y_pred_test
 
--- src/train.py
+++ src/train.py
@@ -1,12 +1,14 @@
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import root_mean_squared_log_error
 
+import numpy as np
+
 def train_model(model, features, train_data):
 
     X = train_data[features]
     y = train_data["trip_duration"]
 
-    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_val, y_train, y_val = train_test_split(X, np.log1p(y), test_size=0.2, random_state=42)
 
     model.fit(X_train, y_train)
     
